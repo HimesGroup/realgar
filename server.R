0954abf579768ae539ea448311e549a107c12c32
Maya Shumyatcher <mayashum@gmail.com>
2016-12-15 15:21:13 -0500

    reduced app loading time by ~30s.

--- server.R
+++ server.R
@@ -1,7 +1,7 @@
 # detach("package:Gviz", unload=TRUE) # this is to keep RStudio happy - run if loading app more than once in same session - keep commented out otherwise
                                     # if load Gviz 2x in same session (i.e. close & re-run app), get "object of type 'closure' is not subsettable" error
                                     # should not be an issue when running app from the website
-cat(file=stderr(), "start package_load\n")                                            
+                                           
 library(shiny)
 library(dplyr)
 library(data.table)
@@ -12,29 +12,22 @@ library(RColorBrewer)
 library(viridis) #needed
 library(DT) #needed
 library(Gviz) #needed
-cat(file=stderr(), "end package_load\n")
+
 
 # load dataset descriptions
 Dataset_Info <- read.csv("databases/microarray_data_infosheet_R.csv")
-cat(file=stderr(), "Dataset_Info\n")
 Dataset_Info$Unique_ID <- apply(Dataset_Info[, c("GEO_ID", "Tissue", "Asthma")], 1, paste, collapse="_")
 
 #load and name datasets
-cat(file=stderr(), as.character(Sys.time()), " start data load\n")
-
 for (i in Dataset_Info$Unique_ID) {
-    cat(file=stderr(), i, "\n")
     assign(i, fread(paste0("databases/microarray_results/", i,".csv"), sep=","))}
 Dataset_Info[is.na(Dataset_Info$PMID),"PMID"] <- ""
-cat(file=stderr(), as.character(Sys.time()), " end data load\n")
 
 tfbs <- fread("databases/tfbs_for_app.txt", header = TRUE, stringsAsFactors = FALSE) #TFBS data from ENCODE - matched to gene ids using bedtools
-cat(file=stderr(), "tfbs\n")
 snp <- fread("databases/grasp_output_for_app.txt", header = TRUE, stringsAsFactors = FALSE) #SNP data from GRASP - matched to gene ids using bedtools
-cat(file=stderr(), "snp\n")
 gene_locations <- fread("databases/gene_positions.txt", header = TRUE, stringsAsFactors = FALSE) #gene location & transcript data from GENCODE
-cat(file=stderr(), "gene_locations\n")
-chrom_bands <- fread("databases/chrom_bands.csv", header=TRUE, stringsAsFactors = FALSE) #chromosome band info for ideogram - faster this way
+
+chrom_bands <- fread("databases/chrom_bands.csv", header=TRUE, stringsAsFactors = FALSE) #chromosome band info for ideogram - makes ideogram load 25 seconds faster
 
 #color tfbs based on binding score - used in tracks
 #create color scheme based on values tfbs binding score & snp p-values
@@ -345,16 +338,10 @@ shinyServer(function(input, output, session) {
       chr <- unique(gene_subs$chromosome)
 
       #chromosome, axis and gene - these tracks show up for all genes
-      cat(file=stderr(), as.character(Sys.time()), " start chrom_track\n")
       bands <- chrom_bands[which(chrom_bands$chrom==chr),]
-      chrom_track <- IdeogramTrack(genome = gen, bands = bands)
-      cat(file=stderr(), as.character(Sys.time()), " end chrom_track\n")
-      cat(file=stderr(), "start axis_track\n")
+      chrom_track <- IdeogramTrack(genome = gen, bands = bands) # formerly slow b/c of chromosome=chr; see https://support.bioconductor.org/p/78881/
       axis_track <- GenomeAxisTrack()
-      cat(file=stderr(), "end axis_track\n")
-      cat(file=stderr(), "start gene_track\n")
       gene_track <- Gviz::GeneRegionTrack(gene_subs, genome = gen, chromosome = chr, name = "Transcripts", transcriptAnnotation="transcript", fill = "royalblue")
-      cat(file=stderr(), "end gene_track\n")
       
       #tfbs and snp track - only present for some genes
       if (nrow(tfbs_subs) > 0) {tfbs_track <- Gviz::AnnotationTrack(tfbs_subs, name="GR binding", fill = tfbs_subs$color, group = " ")}
